90d8a36d855861a2bed9d0ddfa59ca624df81117
zalotores <zalotores@gmail.com>
2023-09-22 16:25:53 +0100

    Completado modelos

--- TPFinalNivel2_Torres/Dominio/Articulo.cs
+++ TPFinalNivel2_Torres/Dominio/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,15 @@ namespace Dominio
 {
     public class Articulo
     {
+        [DisplayName("Código")]
+        public int CodigoArticulo {  get; set; }
+        public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+        public int Marca {  get; set; }
+        [DisplayName("Categoría")]
+        public int Categoria {  get; set; }
+        public string Imagen {  get; set; }
+        public float Precio {  get; set; }
     }
 }
--- TPFinalNivel2_Torres/Dominio/Categoria.cs
+++ TPFinalNivel2_Torres/Dominio/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace Dominio
 {
     internal class Categoria
     {
+        public int Id { get; set; }
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+
+        public override string ToString()
+        {
+            return this.Descripcion;
+        }
     }
 }
